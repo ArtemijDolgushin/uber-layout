88730dd94aecb608ea838be221f6a5dcda4a14dd
boo-mer <artemijdolgushin@gmail.com>
2021-08-08 22:42:21 +0300

    add nav links, contact info and background image

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,12 @@
 import '../styles/reset.css';
 import '../styles/index.scss';
-
+import content__contacts__image__source from '../img/uber_mail.png';
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('webpack starterkit');
+const content__contacts__image= document.querySelector('.content__contacts__image-mail');
+const image = new Image(285,32);
+image.src = content__contacts__image__source;
+content__contacts__image.appendChild(image);
+//content__contacts__image.src = content__contacts__image__source;
